924ada63e620acfaf3feee1aefdb560598899ae2
rrussoneu <rrussoneu@gmail.com>
2023-09-17 22:04:11 -0400

    project initial upload

---
+++ README.md
@@ -0,0 +1,1 @@
+A Full Stack Personal Dashboard
---
+++ RaphsDashApi/RaphsDashApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RaphsdashappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'RaphsDashApp'
